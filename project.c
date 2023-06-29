12a302a9bc3894d417940986992e6ce92ce51f1a
supreet-m <131605847+supreet-m@users.noreply.github.com>
2023-06-29 23:41:18 +0530

    Add files via upload

--- project.c
+++ project.c
@@ -2,47 +2,59 @@
 #include<string.h>
 #include<conio.h>
 
-struct customer{
+typedef struct{
 
     char name[50];
-    char address[100];
+    char address[50];
     char phone_no[10];
-    int udhar;
+    int amount_to_pay;
 
-};
+} customer;
 
-struct item{
+typedef struct{
     char product_no[50];
-    char product_name[100];
+    char product_name[50];
     int quantity;
     int price;
-};
+} item;
+
 
 void add_new_products(){
 
-    FILE *products;
-    products=fopen("products.txt","a");
-    printf("Enter number of products : ");
-    int num;
-    scanf("%d",&num);
-    for(int i=0; i<num; i++){
+    FILE *products; //file pointer
+    products=fopen("products.csv","a");
+    if(products == NULL){
+        printf("Errror opening file.\n");
+        //exit(0);
+    }
+    else{
+        printf("Enter number of products : ");
+        int num; //number of records
+        scanf("%d",&num);
+        item prdct[num];  // prdct is stucture name of datatype item
+        for(int i=0; i<num; i++){
 
-        char product_no[50];
-        char product_name[100];
-        int quantity;
-        int price;
+            printf("Enter product number : ");
+            scanf("%s",prdct[i].product_no);
+            printf("Enter product_name : ");
+            scanf("%s",prdct[i].product_name);
+            printf("Enter quantity : ");
+            scanf("%d",&prdct[i].quantity);
+            printf("Enter price : ");
+            scanf("%d",&prdct[i].price);
 
-        printf("Enter product number : ");
-        scanf("%s",product_no);
-        printf("Enter product_name : ");
-        scanf("%s",product_name);
-        printf("Enter quantity : ");
-        scanf("%d",&quantity);
-        printf("Enter price : ");
-        scanf("%d",&price);
+            fprintf(products,"%s,%s,%d,%d\n",
+                            prdct[i].product_no,
+                            prdct[i].product_name,
+                            prdct[i].quantity,
+                            prdct[i].price);
 
-        fprintf(products,"%s \t %s \t %d \t %d\n", product_no , product_name , quantity , price);
-
+            if(ferror(products)){
+                printf("Error writing to file");
+                //exit(0);
+            }
+            
+        }
     }
 
     fclose(products);
@@ -50,62 +62,249 @@ void add_new_products(){
 
 void newCustomer(){
 
-    FILE *customer;
-    customer=fopen("customer.txt","a");
+    FILE *file;
+    file = fopen("customer_file.csv","a");
+    if(file == NULL) printf("Error opening file.\n");
+    else
+    {
+        customer cstmr;
+        printf("Enter name of the customer : ");
+        scanf("%s",cstmr.name);
+        printf("Enter address of customer : ");
+        scanf("%s",cstmr.address);
+        printf("Enter phone_no of customer : ");
+        scanf("%s",cstmr.phone_no);
+        cstmr.amount_to_pay=0;
+        // printf("Enter amount_to_pay : ");
+        // scanf("%d",&cstmr.amount_to_pay);
 
-    char customer_name[50];
-    char address[100];
-    char phone_no[10];
-    int due_amount;
+        //fprintf(file,"%s\t\t%s\t\t%s\t\t%d\n",customer_name , address , phone_no , due_amount);
 
-    printf("Enter name of the customer : ");
-    scanf("%s",customer_name);
-    printf("Enter address of customer : ");
-    scanf("%s",address);
-    printf("Enter phone_no of customer : ");
-    scanf("%s",phone_no);
-    printf("Enter due_amount : ");
-    scanf("%d",&due_amount);
+        fprintf(file,"%s,%s,%s,%d",
+                    cstmr.name,
+                    cstmr.address,
+                    cstmr.phone_no,
+                    cstmr.amount_to_pay);
 
-    fprintf(customer,"%s \t %s \t %s \t %d\n",customer_name , address , phone_no , due_amount);
+        fclose(file);
+    }
+}
 
-    fclose(customer);
+void modify_products(){
+    FILE *file;
+    file=fopen("products.csv","a+");
+    if(file==NULL) {
+        printf("Error opening file.\n");
+    }
+    else{
+
+    }
+
+
 }
 
 void cart(){
 
-    FILE *cart;
-    cart=fopen("cart.txt","w");
-    printf("Enter products_no of product to buy : ");
-    char product_no[50];
-    scanf("s",product_no);
+    FILE *prdct;
+    prdct=fopen("products.csv","r+");
+    int total_amount=0;
+    item buy_items[50];   //to store the purchased items
+    item array[50];       // to strore the items available
 
-    fclose(cart);
+
+    int read=0; 
+    int records=0;
+    do
+    {
+        read=fscanf(prdct,"%49[^,],%49[^,],%d,%d",
+                array[records].product_no,
+                array[records].product_name,
+                &array[records].quantity,
+                &array[records].price);
+        if(read==4) records++;
+        
+        if(read != 4 &&  (!feof(prdct))){
+            printf("File formatting issue.\n");
+        }
+
+        if(ferror(prdct)) {
+            printf("Error reading file.\n");
+        }
+    }
+    while(!feof(prdct));
+
+
+    int index = 0;  //index of array of structs (buy_items)
+    
+    buy: printf("");
+    item product1;  //product1 is structure name of datatype item
+    printf("Enter products_no of product to buy : ");
+    scanf("%s",product1.product_no);
+    printf("Enter product name : ");
+    scanf("%s",product1.product_name);
+    printf("Enter quantity : ");
+    scanf("%d",&product1.quantity);
+
+    for(int i=0; i<records; i++){
+        if(strcmp(array[i].product_no , product1.product_no)){    //string comparision
+            if(product1.quantity > array[i].quantity) {
+                printf("Not enoungh quantity available of required product.\n");
+            }
+            else {
+                product1.price = array[i].price;
+                buy_items[index] = product1;
+                index++;
+                total_amount += (array[i].price * product1.quantity);
+                break;
+            }
+        }
+    }
+    char ch[2];
+    printf("Do you want to buy more(y/n) : ");
+    scanf("%s",ch);
+    
+    if(!strcmp(ch,"y")){
+        goto buy;
+    }
+
+
+    
+    FILE *buy;
+    buy = fopen("bought_items.csv","w");
+    if(buy == NULL) {
+        printf("Error in opening file.\n");
+    }
+    else {
+        for(int i=0; i<index; i++){
+            fprintf(buy,"%s,%s,%d,%d",
+                    buy_items[i].product_no,
+                    buy_items[i].product_name,
+                    buy_items[i].quantity,
+                    buy_items[i].price);
+        }
+    }
+
+    modify_products();   // update the available products quantity  (not completed yet)
+    
+    fclose(buy);
+    fclose(prdct);
 } 
+
+void bill(){
+
+}
 
 void display_available_products(){
 
     FILE *products;
-    products=fopen("products,txt","r");
-    printf("Product No\t Product Name\t Quantity\t Price\n");
+    products=fopen("products.csv","r");
+    if(products==NULL){
+        printf("Error opening file.\n");
+    }
+    else{
+        printf("Product No\tProduct Name\tQuantity\tPrice\n");
 
-    struct item item1;
-    int st;
-    char str[50];
-    while(fscanf(products,"%*s %s %*s %*s",str)==1){
-        printf("str == %s\n",str);
-        //printf("%s %s %d %d",item1.product_no, item1.product_name, item1.quantity, item1.price);
+        item item1[100];   //array of structs to store products 
+
+        int read = 0;
+        int records = 0;
+        do{
+            //printf("tugu1");
+            read=fscanf(products,"%49[^,],%49[^,],%d,%d",
+                            item1[records].product_no,
+                            item1[records].product_name,
+                            &item1[records].quantity,
+                            &item1[records].price);
+
+            if(read==4) records++;
+             //printf("tugu1");
+            if(read != 4 &&  (!feof(products))){
+                printf("File formatting issue.\n");
+            }
+
+            if(ferror(products)) {
+                printf("Error reading file.\n");
+            }
+        }
+        while(!feof(products));
+
+        //printf("%d",records);
+        for(int i=0; i<records; i++){
+            printf("%s\t\t%s\t\t%d\t\t%d",
+                    item1[i].product_no,
+                    item1[i].product_name,
+                    item1[i].quantity,
+                    item1[i].price);
+        }
+
     }
 
     fclose(products);
 }
 
-int main(){
+void administrator(){
+    printf("Please Enter an Option: ");
+    printf("01.Add New Products");
+    printf("02.Display Available Products");
+    printf("03.Main Menu");
 
+    int choice;
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case 1:
+        add_new_products();
+        break;
+    case 2:
+        display_available_products();
+        break;
+    case 3:
+        mainMenu();
+        break;
+    default:
+        break;
+    }
+
+}
+
+void customer_func(){
+
+}
+
+void mainMenu(){
     printf("                        Welcome to IITP departmental store\n");
 
-    
-    add_new_products();
-    display_available_products();
+    printf("01. ADMINISTRATOR\n");
+	printf("02. CUSTOMER\n");
+	printf("03. EXIT\n");
+
+	printf("Please Enter an Option: ");
+    int choice;
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1:
+            administrator();
+            break;
+        case 2:
+            customer_func();
+        case 3:
+            printf("     !!!! THANK YOU !!!!     \n");
+            printf("         Visit again   :)       ");
+            break;
+        default:
+            printf("Wrong choice\n Please select a valid choice\n");
+            break;
+    }
+
+}
+
+int main(){
+
+    cart();
+    //mainMenu();
+    //add_new_products();
+    //newCustomer();
+    //display_available_products();
 
 }
